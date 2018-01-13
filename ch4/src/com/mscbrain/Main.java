016714782c6e4821c9ea6290faa3ad101292f7e8
Jeong Eunwoo <zenic88@naver.com>
2018-01-13 23:14:38 +0900

    4-3 등록

--- ch4/src/com/mscbrain/Main.java
+++ ch4/src/com/mscbrain/Main.java
@@ -1,12 +1,13 @@
 package com.mscbrain;
 
-import com.mscbrain.practice.ex02;
+import com.mscbrain.practice.ex03;
 
 public class Main {
 
   public static void main(String[] args) {
     // write your code here
     // ex01.ex1();
-    ex02.ex2();
+    // ex02.ex2();
+    ex03.ex3();
   }
 }
---
+++ ch4/src/com/mscbrain/practice/ex03.java
@@ -0,0 +1,17 @@
+package com.mscbrain.practice;
+
+public class ex03 {
+
+  public static void ex3() {
+
+    int sum = 0;
+
+    for (int i = 1; i < 11; i++) {
+      sum += (i * (i + 1)) / 2;
+    }
+
+    System.out.println(sum);
+
+  }
+
+}
