647524b768f5070cc1adaf38afc100d7309d46a8
Jeong Eunwoo <zenic88@naver.com>
2018-01-20 21:04:24 +0900

    6-3 문제

---
+++ ch6/ex03-04/src/com/damdev/Main.java
@@ -0,0 +1,20 @@
+package com.damdev;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // write your code here
+
+  }
+}
+
+class Student {
+
+  String name;
+  int ban;
+  int no;
+  int kor;
+  int eng;
+  int math;
+  
+}
