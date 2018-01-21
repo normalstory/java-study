23eeedeaa66c6b1298eb429a84d865885e772afa
Jeong Eunwoo <zenic88@naver.com>
2018-01-21 21:18:03 +0900

    6-3, 6-4, 6-5 문제 풀이

---
+++ ch6/ex03-05/src/com/damdev/Main.java
@@ -0,0 +1,58 @@
+package com.damdev;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // write your code here
+    Student s = new Student();
+    s.name = "홍길동";
+    s.ban = 1;
+    s.no = 1;
+    s.kor = 100;
+    s.eng = 60;
+    s.math = 76;
+    System.out.println("이름:" + s.name);
+    System.out.println("총점:" + s.getTotal());
+    System.out.println("평균:" + s.getAverage());
+
+    s = new Student("홍길동", 1, 1, 100, 60, 76);
+
+    System.out.println(s.info());
+  }
+}
+
+class Student {
+
+  String name;
+  int ban;
+  int no;
+  int kor;
+  int eng;
+  int math;
+
+  Student() {
+  }
+
+  Student(String name, int ban, int no, int kor, int eng, int math) {
+    this.name = name;
+    this.ban = ban;
+    this.no = no;
+    this.kor = kor;
+    this.eng = eng;
+    this.math = math;
+  }
+
+  int getTotal() {
+    return kor + eng + math;
+  }
+
+  float getAverage() {
+    return (int) ((((kor + eng + math) / 3f) * 10) + 0.5) / 10f;
+  }
+
+  String info() {
+    return name + "," + ban + "," + no + "," + kor + "," + eng + "," + math + "," + getTotal() + ","
+      + getAverage();
+  }
+
+}
