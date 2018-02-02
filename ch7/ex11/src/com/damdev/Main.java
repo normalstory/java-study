9ebf5c7927bd77481929c9648f77554703c41a31
Jeong Eunwoo <zenic88@naver.com>
2018-02-02 23:57:27 +0900

    7-11 문제 풀이

---
+++ ch7/ex11/src/com/damdev/Main.java
@@ -0,0 +1,53 @@
+package com.damdev;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // write your code here
+    MyTv2 t = new MyTv2();
+    t.setChannel(10);
+    System.out.println("CH:" + t.getChannel());
+    t.setChannel(20);
+    System.out.println("CH:" + t.getChannel());
+    t.gotoPrevChannel();
+    System.out.println("CH:" + t.getChannel());
+    t.gotoPrevChannel();
+    System.out.println("CH:" + t.getChannel());
+  }
+}
+
+class MyTv2 {
+
+  private boolean isPowerOn;
+  private int channel;
+  private int volume;
+  private int prevChannel;
+
+  final int MAX_VOLUME = 100;
+  final int MIN_VOLUME = 0;
+  final int MAX_CHANNEL = 100;
+  final int MIN_CHANNEL = 1;
+
+  public void setChannel(int channel) {
+    this.prevChannel = this.channel;
+    this.channel = channel;
+  }
+
+  public int getChannel() {
+    return this.channel;
+  }
+
+  public void setVolume(int volume) {
+    this.volume = volume;
+  }
+
+  public int getVolume() {
+    return this.volume;
+  }
+
+  public void gotoPrevChannel() {
+    int temp = this.channel;
+    this.channel = this.prevChannel;
+    this.prevChannel = temp;
+  }
+}
