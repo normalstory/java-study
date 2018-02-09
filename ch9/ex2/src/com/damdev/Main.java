e70c6b000fe2aa8bc68715c9749516fcb43df928
Jeong Eunwoo <zenic88@naver.com>
2018-02-09 21:38:56 +0900

    9-2 문제 풀이

---
+++ ch9/ex2/src/com/damdev/Main.java
@@ -0,0 +1,45 @@
+package com.damdev;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // write your code here
+    Point3D p1 = new Point3D(1, 2, 3);
+    Point3D p2 = new Point3D(1, 2, 3);
+
+    System.out.println(p1);
+    System.out.println(p2);
+    System.out.println("p1 == p2 ? " + (p1 == p2));
+    System.out.println("p1.equals(p2) ? " + p1.equals(p2));
+  }
+}
+
+class Point3D {
+
+  int x, y, z;
+
+  Point3D(int x, int y, int z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+
+  Point3D() {
+    this(0, 0, 0);
+  }
+
+  public boolean equals(Object obj) {
+
+    if (obj instanceof Point3D) {
+      Point3D p = (Point3D) obj;
+
+      return p.x == x && p.y == y && p.z == z;
+    }
+
+    return false;
+  }
+
+  public String toString() {
+    return "[" + x + "," + y + "," + z + "]";
+  }
+}
