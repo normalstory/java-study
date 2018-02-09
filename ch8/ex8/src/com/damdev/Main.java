81491b4bfbf8a045d5374fbef1120326d8f8e1b5
Jeong Eunwoo <zenic88@naver.com>
2018-02-09 21:07:41 +0900

    8-8 문제 풀이

---
+++ ch8/ex8/src/com/damdev/Main.java
@@ -0,0 +1,36 @@
+package com.damdev;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // write your code here
+    int answer = (int) (Math.random() * 100) + 1;
+    int input = 0;
+    int count = 0;
+
+    do {
+      count++;
+      System.out.print("1과 100사이의 값을 입력하세요 : ");
+
+      try {
+        input = new Scanner(System.in).nextInt();
+      } catch (InputMismatchException e) {
+        System.out.println("유효하지 않은 값입니다. 다시 값을 입력해주세요.");
+        continue;
+      }
+
+      if (answer > input) {
+        System.out.println("더 큰 수를 입력하세요.");
+      } else if (answer < input) {
+        System.out.println("더 작은 수를 입력하세요.");
+      } else {
+        System.out.println("맞췄습니다.");
+        System.out.println("시도횟수는 " + count + "번입니다.");
+        break;
+      }
+    } while (true);
+  }
+}
