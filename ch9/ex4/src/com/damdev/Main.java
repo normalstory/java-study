f6ae758a51f653bcb101ae09f8fd3382a4c8cccd
Jeong Eunwoo <zenic88@naver.com>
2018-02-09 22:00:22 +0900

    9-4 문제 풀이

---
+++ ch9/ex4/src/com/damdev/Main.java
@@ -0,0 +1,18 @@
+package com.damdev;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // write your code here
+    printGraph(new int[]{3, 7, 1, 4}, '*');
+  }
+
+  static void printGraph(int[] dataArr, char ch) {
+    for (int i = 0; i < dataArr.length; i++) {
+      for (int j = 0; j < dataArr[i]; j++) {
+        System.out.print(ch);
+      }
+      System.out.println(dataArr[i]);
+    }
+  }
+}
